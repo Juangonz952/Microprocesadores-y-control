8805fb231245fd4aadf1ddc13f1dc60fc9d5bdcd
Juangonz952 <jgonzalezmartin@itba.edu.ar>
2025-04-08 12:16:26 -0300

    pruebas

--- pruebas.c
+++ pruebas.c
@@ -1,41 +1,19 @@
 #include <stdio.h>
-#include <stdint.h> 
-#include <string.h>
 #include <stdlib.h>
 
-int Es_Valido(char c[20]){
-    int index;
-    for(index = 0; index < 20 && c[index] != '\0'; index++){
-        if(c[index] < '0' || c[index] > '9'){
-            return 1;
-        }
-    }
-    return 0;
-}
-int main(){
-    char c[20];
-    int a;
-    fgets(c, 20, stdin);
-    for(int i=0; i<20; i++){
-        if(c[i] == '\n'){
-            c[i] = '\0';
-        }
-    }
-    for(int i= 0; i <20; i++){
-        if( c[i]<'0' || c[i] > '9'){
-            printf("no ingreso un numero");
-            break;
-        }
-    }
-    printf("%d",Es_Valido(c));
-    printf("%s", c);
-    if(strcmp(c,"EXIT") == 0){
-        printf("Se ingreso EXIT\n");
-    }
-    else{
-        printf("No se ingreso EXIT\n");
-    }
-    a = atoi(c);
-    printf("%d\n", a);
+int M2D[][5] = {
+    {1, 2, 3, 4},
+    {5,6, 7, 8},
+    {9, 10, 11, 12},
+};
+
+int *p0 = M2D[0]; 
+int *p1 = M2D[1]; 
+
+int main(void){
+    
+    printf("S0 = %d\n", p0[4]);
+    printf("S1 = %d\n", p1[5]);
+    
     return 0;
 }
